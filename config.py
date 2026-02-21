6f46a5bffc1eb5e65a09c2022ab40a6e66f0a3de
Carlos <carlos@askme.com>
2026-02-21 19:28:14 +0100

    Bump version to v0.8.4

--- config.py
+++ config.py
@@ -14,7 +14,7 @@ class Config:
     # 1. INFORMACIÓN DE LA APLICACIÓN
     # ========================================================================
     APP_NAME = "Innerpix Lab"
-    APP_VERSION = "0.8.1"
+    APP_VERSION = "0.8.4"
     APP_VERSION_SUFFIX = "alpha"  # "beta", "rc1", "" (empty for stable)
     APP_AUTHOR = "SafeToolHub"
     APP_CONTACT = "safetoolhub@protonmail.com"
